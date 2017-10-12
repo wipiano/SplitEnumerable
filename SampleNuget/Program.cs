f33b39cfd269710b3c60e2bc70527fac426fe06d
Kohei Hakoishi <wipiano@gmail.com>
2017-10-13 03:11:14 +0900

    add sample using nuget

---
+++ SampleNuget/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Sample
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var splitted = EnumerateNumber().Take(100).Split(20);
+        
+            foreach (IEnumerable<int> chunk in splitted)
+            {
+                if (chunk.Any())
+                {
+                    Console.WriteLine(string.Join(", ", chunk));
+                }
+                else
+                {
+                    Console.WriteLine("empty!");
+                }
+            }
+        }
+        
+        private static IEnumerable<int> EnumerateNumber()
+        {
+            int n = 0;
+            while (true)
+            {
+                yield return n++;
+            }
+        }
+    }
+}
