4ece495f70731d10fb56d017fd76784d9a167d98
Kohei Hakoishi <wipiano@gmail.com>
2017-10-13 01:42:36 +0900

    参照を nuget に変更

--- Sample/Program.cs
+++ Sample/Program.cs
@@ -8,14 +8,14 @@ namespace Sample
     {
         static void Main(string[] args)
         {
-            var splitted = EnumerateNumber().Take(77).Split(5);
-
+            var splitted = EnumerateNumber().Take(77).Split(10);
+        
             foreach (IEnumerable<int> chunk in splitted)
             {
                 Console.WriteLine(string.Join(", ", chunk));
             }
         }
-
+        
         private static IEnumerable<int> EnumerateNumber()
         {
             int n = 0;
