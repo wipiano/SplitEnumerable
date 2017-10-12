1958131e4f90762c9fa24dad185edb9e52a95823
Kohei Hakoishi <wipiano@gmail.com>
2017-10-13 01:10:52 +0900

    initial commit

---
+++ EnumerableSplitHelper/EnumerableExtensions.cs
@@ -0,0 +1,57 @@
+﻿using System.Collections.Generic;
+
+namespace System.Linq
+{
+    public static class EnumerableExtensions
+    {
+        /// <summary>
+        /// コレクションを size ごとに分割して列挙します
+        /// </summary>
+        /// <param name="that">分割するコレクション</param>
+        /// <param name="size">分割後のサイズ</param>
+        public static IEnumerable<IEnumerable<T>> Split<T>(this IEnumerable<T> that, int size)
+        {
+            if (that == null)
+            {
+                throw new ArgumentNullException(nameof(that));
+            }
+
+            // はじめに size ぶんのバッファを確保
+            T[] buffer = new T[size];
+
+            bool hasNext = true;
+            
+            // バッファをつぎの要素で埋める関数
+            // 何個埋めたか返す
+            int FillNext(IEnumerator<T> enumerator)
+            {
+                int count = 0;
+                while (count < size)
+                {
+                    if (enumerator.MoveNext())
+                    {
+                        buffer[count++] = enumerator.Current;
+                    }
+                    else
+                    {
+                        hasNext = false;
+                        break;
+                    }
+                }
+
+                return count;
+            }
+
+            using (var e = that.GetEnumerator())
+            {
+                while (hasNext)
+                {
+                    int length = FillNext(e);
+                    var result = new T[length];
+                    Array.Copy(buffer, result, length);
+                    yield return result;
+                }
+            }
+        }
+    }
+}
---
+++ Sample/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Sample
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var splitted = EnumerateNumber().Take(77).Split(5);
+
+            foreach (IEnumerable<int> chunk in splitted)
+            {
+                Console.WriteLine(string.Join(", ", chunk));
+            }
+        }
+
+        private static IEnumerable<int> EnumerateNumber()
+        {
+            int n = 0;
+            while (true)
+            {
+                yield return n++;
+            }
+        }
+    }
+}
